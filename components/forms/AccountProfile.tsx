bdc46b9dec488a8d9c29c94547feefe47dd3421e
Elwood Bond <elwood.bond@gmail.com>
2023-09-14 20:14:49 -0400

    onboarding formatting, config files added, 1:37:48

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -35,10 +35,10 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     const form = useForm({
         resolver: zodResolver(userValidation),
         defaultValues: {
-            profile_photo: "",
-            name: "",
-            username: "",
-            bio: ""
+            profile_photo: user?.image || "",
+            name: user?.name || "",
+            username: user?.username|| "",
+            bio: user?.bio || ""
         },
     })
 
@@ -98,11 +98,11 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             control={form.control}
             name="name"
             render={({ field }) => (
-              <FormItem className="flex items-center gap-3 w-full">
+              <FormItem className="flex flex-col w-full gap-3">
                 <FormLabel className="text-base-semibold text-light-2">
                     Name        
                 </FormLabel>
-                <FormControl className="flex-1 text-base-semibold text-gray-200">
+                <FormControl>
                   <Input 
                     type="text"
                   className="account-form_input no-focus"
@@ -117,11 +117,11 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             control={form.control}
             name="username"
             render={({ field }) => (
-              <FormItem className="flex items-center gap-3 w-full">
+              <FormItem className="flex flex-col w-full gap-3">
                 <FormLabel className="text-base-semibold text-light-2">
                     Username        
                 </FormLabel>
-                <FormControl className="flex-1 text-base-semibold text-gray-200">
+                <FormControl>
                   <Input 
                     type="text"
                   className="account-form_input no-focus"
@@ -136,12 +136,12 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
             control={form.control}
             name="Bio"
             render={({ field }) => (
-              <FormItem className="flex items-center gap-3 w-full">
+              <FormItem className="flex flex-col w-full gap-3">
                 <FormLabel className="text-base-semibold text-light-2">
-                    Bio       
+                    Bio        
                 </FormLabel>
                 <FormControl className="flex-1 text-base-semibold text-gray-200">
-                  <Textarea 
+                  <Textarea
                     rows={10}
                   className="account-form_input no-focus"
                   {...field}
